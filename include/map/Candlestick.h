16496d6b28f7d724bdac034a3e0af81b2686faa6
LuvoZulu <zululuvo03@gmail.com>
2026-08-07 11:40:33 +0200

    update(Candlestick & tick): improved design for candlesticks and ticks, and clear relationship

--- include/map/Candlestick.h
+++ include/map/Candlestick.h
@@ -1,13 +1,27 @@
-#ifndef TICK_H
-#define TICK_H
+#ifndef CANDLESTICK_H
+#define CANDLESTICK_H
 
 #include <chrono> // std::chrono
+#include <fstream> // std::ifstream
+#include <filesystem> // std::filesystem::path
+#include <queue> //std::queue
+#include <vector> // std::vector
 
 #include "logging/Logging.h" // map::logging::Logger
 
 
 
-namespace map::market_data{
+namespace map::market_data {
+
+    enum class Timeframe {
+        TICK = 0, // only here for design and also live market updates, which exclude verbose info
+        M1 = 1,
+        M5 = 2,
+        M15 = 3,
+        M30 = 4,
+        H1 = 5,
+        H4 = 6,
+    };
 
 	//<DATE>	<TIME>	<OPEN>	<HIGH>	<LOW>	<CLOSE>	<TICKVOL>	<VOL>	<SPREAD>
     struct Candlestick {
@@ -40,7 +54,7 @@ namespace map::market_data{
     // <DATE>	<TIME>	<BID>	<ASK>	<LAST>	<VOLUME>	<FLAGS>
     // TODO: This class I am planning on using to validate my tick class above. This is the actual tick class, that gets
     //       market events each time they change. The above is the tick every minute,5 minutes, etc
-    struct Tick_v {
+    struct Tick {
         std::chrono::milliseconds   m_time;
         std::chrono::year_month_day m_date;
         double m_bid;
@@ -49,7 +63,7 @@ namespace map::market_data{
         double m_volume;
         size_t flags;
 
-        Tick_v(std::chrono::milliseconds time, std::chrono::year_month_day date,double bid, double ask, double last,
+        Tick(std::chrono::milliseconds time, std::chrono::year_month_day date,double bid, double ask, double last,
                 double volume, size_t flags
         ) :
             m_time{time}, m_date{date}, m_bid{bid}, m_ask{ask}, m_last{last}, m_volume{volume} , m_flags{flags}
@@ -60,7 +74,20 @@ namespace map::market_data{
     class CandleStickBuilder {
     public:
         CandleStickBuilder(){}
+        Candlestick get_candlestick(Timeframe timeframe, std::vector<Timeframe> tfs = {});
         ~CandleStickBuilder(){}
+    private:
+        Tick build_tick(std::string& path); // temporary - bad design
+        void make_candlesticks(std::string& path);
+
+        std::queue<Tick> ticks_;
+        std::vector<Tick> processed_ticks;
+
+        std::vector<Candlestick> minute_candlesticks;
+        std::vector<Candlestick> minute_15_candlesticks;
+        std::vector<Candlestick> minute_30_candlesticks;
+        std::vector<Candlestick> hour_candlesticks;
+        std::vector<Candlestick> hour_4_candlesticks;
     };
 }
 
@@ -73,4 +100,4 @@ namespace map::market_data{
 
 
 
-#endif // !TICK_H
+#endif // !CANDLESTICK_H
---
+++ src/map/Candlestick.cpp
@@ -0,0 +1,19 @@
+#include "Candlestick.h"
+
+
+namespace map::market_data {
+	Candlestick CandleStickBuilder::get_candlestick(Timeframe timeframe, std::vector<Timeframe> tfs)
+	{
+		return Candlestick();
+	}
+
+	Tick CandleStickBuilder::build_tick(std::string& path)
+	{
+		return Tick();
+	}
+
+	void CandleStickBuilder::make_candlesticks(std::string& path)
+	{
+
+	}
+}
