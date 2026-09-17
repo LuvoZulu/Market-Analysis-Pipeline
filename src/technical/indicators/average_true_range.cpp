bb37c220dcec2c9af622fe654699bf094b9e3b91
LuvoZulu <zululuvo03@gmail.com>
2026-09-17 23:19:17 +0200

    feat(average_true_range): Calculate the average true range give from date , to date or both and times

--- src/technical/indicators/average_true_range.cpp
+++ src/technical/indicators/average_true_range.cpp
@@ -16,17 +16,17 @@ namespace map::indicators
         double sum_tr = 0.0;
         size_t count = 0;
 
-        auto prev = data.cbegin();
-        auto curr = std::next(prev);
+        auto prev_stick = data.cbegin();
+        auto curr_stick = std::next(prev_stick);
 
-        while (curr != data.cend() && curr->m_date != target_date) {
-            const double tr = std::max({std::abs(curr->m_high - curr->m_low),std::abs(curr->m_high - prev->m_close),std::abs(curr->m_low - prev->m_close)});
+        while (curr_stick != data.cend() && curr_stick->m_date != target_date) {
+            const double tr = std::max({std::abs(curr_stick->m_high - curr_stick->m_low),std::abs(curr_stick->m_high - prev_stick->m_close),std::abs(curr_stick->m_low - prev_stick->m_close)});
 
             sum_tr += tr;
             ++count;
 
-            prev = curr;
-            ++curr;
+            prev_stick = curr_stick;
+            ++curr_stick;
         }
 
         return count > 0 ? sum_tr / count : 0.0;
@@ -41,23 +41,22 @@ namespace map::indicators
         double sum_tr = 0.0;
         size_t count = 0;
 
-        auto prev = data.cbegin();
-        auto curr = std::next(prev);
+        auto prev_stick = data.cbegin();
+        auto curr_stick = std::next(prev_stick);
 
-        while (curr != data.cend()) {
+        while (curr_stick != data.cend()) {
 
-            if (curr->m_date > duration ||
-                (curr->m_date == duration && curr->m_time > to_time)) {
+            if (curr_stick->m_date > duration || (curr_stick->m_date == duration && curr_stick->m_time > to_time)) {
                 break;
             }
 
-            const double tr = std::max({std::abs(curr->m_high - curr->m_low),std::abs(curr->m_high - prev->m_close),std::abs(curr->m_low - prev->m_close)});
+            const double tr = std::max({std::abs(curr_stick->m_high - curr_stick->m_low),std::abs(curr_stick->m_high - prev_stick->m_close),std::abs(curr_stick->m_low - prev_stick->m_close)});
 
             sum_tr += tr;
             ++count;
 
-            prev = curr;
-            ++curr;
+            prev_stick = curr_stick;
+            ++curr_stick;
         }
 
         return count > 0 ? sum_tr / count : 0.0;
@@ -72,31 +71,63 @@ namespace map::indicators
         double sum_tr = 0.0;
         size_t count = 0;
 
-        auto prev = data.cbegin();
-        auto curr = std::next(prev);
+        auto prev_stick = data.cbegin();
+        auto curr_stick = std::next(prev_stick);
 
-        while (curr != data.cend()) {
+        while (curr_stick != data.cend()) {
             
-            if (curr->m_date < from) {
+            if (curr_stick->m_date < from) {
+                prev_stick = curr_stick;
+                ++curr_stick;
                 continue;
-            }else if (curr->m_date > to) {
+            }else if (curr_stick->m_date > to) {
                 break;
             }
 
-            const double tr = std::max({std::abs(curr->m_high - curr->m_low),std::abs(curr->m_high - prev->m_close),std::abs(curr->m_low - prev->m_close)});
+            const double tr = std::max({std::abs(curr_stick->m_high - curr_stick->m_low),std::abs(curr_stick->m_high - prev_stick->m_close),std::abs(curr_stick->m_low - prev_stick->m_close)});
 
             sum_tr += tr;
             ++count;
 
-            prev = curr;
-            ++curr;
+            prev_stick = curr_stick;
+            ++curr_stick;
         }
 
         return count > 0 ? sum_tr / count : 0.0;
 	}
 
-	double Atr::get_average(std::vector<map::market_data::Candlestick>&, std::chrono::year_month_day& from_date, std::chrono::milliseconds& from_time, std::chrono::year_month_day& to, std::chrono::milliseconds& to_time) 
+	double Atr::get_average(std::vector<map::market_data::Candlestick>& data, std::chrono::year_month_day& from_date, std::chrono::milliseconds& from_time, std::chrono::year_month_day& to_date, std::chrono::milliseconds& to_time) 
 	{
-		return 0.0;
+        if (data.size() < 2) {
+            return 0.0;
+        }
+
+        double sum_tr = 0.0;
+        size_t count = 0;
+
+        auto prev_stick = data.cbegin();
+        auto curr_stick = std::next(prev_stick);
+
+        while (curr_stick != data.cend()) {
+
+            if ((curr_stick->m_date < from_date) || (curr_stick->m_time < to_time)) {
+                prev_stick = curr_stick;
+                ++curr_stick;
+                continue;
+            }
+            else if ((curr_stick->m_date > to_date) && (curr_stick->m_time > to_time)) {
+                break;
+            }
+
+            const double tr = std::max({ std::abs(curr_stick->m_high - curr_stick->m_low),std::abs(curr_stick->m_high - prev_stick->m_close),std::abs(curr_stick->m_low - prev_stick->m_close) });
+
+            sum_tr += tr;
+            ++count;
+
+            prev_stick = curr_stick;
+            ++curr_stick;
+        }
+
+        return count > 0 ? sum_tr / count : 0.0;
 	}
 }
